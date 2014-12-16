81770d371bd1f5c3630500c8f40fda9d69d5a155
Moski Doski <abushaikh@gmail.com>
2014-12-16 23:30:47 +0200

    dropping the min coverage to 90

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'coveralls'
 SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
 SimpleCov.start do
   add_filter '/spec/'
-  minimum_coverage(99.61)
+  minimum_coverage(90)
 end
 
 require 'flood'
