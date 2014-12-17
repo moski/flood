cd9ffec42264e667e5d1f7e8c649271af7db5c92
Moski Doski <abushaikh@gmail.com>
2014-12-17 18:34:36 +0200

    fixing the white space

--- spec/flood/identity_spec.rb
+++ spec/flood/identity_spec.rb
@@ -1,5 +1,4 @@
 describe Flood::Identity do
-
   describe '#initialize' do
     it 'raises an IndexError when info is not specified' do
       expect { Flood::Identity.new }.to raise_error(IndexError)
